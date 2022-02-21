eda0b9f2d4cbbacc775afec7d99357868dd2ebd0
Latika Sehgal <slati@leaseplan.com>
2022-02-21 12:29:06 +0100

    Updated damage logic

--- src/main/scala/Dragon.scala
+++ src/main/scala/Dragon.scala
@@ -1,21 +1,34 @@
 import java.util.concurrent.atomic.AtomicInteger
-
+import java.util.logging.{Level, Logger}
 
 case class Dragon(hitPoints: Int, isAlive: Boolean)
+case class DragonAndResult(dragon: Dragon, message: String)
 
-object DragonKata extends App {
+object Dragon {
   val damageCount:AtomicInteger = new AtomicInteger()
-  var hitPoints = 20
+  val logger = Logger.getLogger("Dragon")
 
-  def doDamage(damage: Int): Dragon = {
-    val newHitPoints = hitPoints - damage
+  def handleErrors:(Attack, Exception) => DragonAndResult = {
+    case (attack, e: Exception) => {
+      logger.log(Level.SEVERE, "Unexpected error damaging " + this + " for " + attack.damage + " hitpoints", e)
+      throw e
+    }
+  }
+}
+
+
+case class Attack(dragon: Dragon, damage: Int)
+
+object Attack extends App {
+  def doDamage(dragon: Dragon, damage: Int): Dragon = {
+    val newHitPoints = dragon.hitPoints - damage
     if (newHitPoints <= damage) {
       println("Dragon was hit for "+damage+" and is now dead.")
       Dragon(0, false)
     }
     else {
       damageCount.incrementAndGet()
-      println("Dragon was damage by "+damage+" hitpoints. Current hitpoints: "+newHitPoints)
+      println("Dragon was damaged by "+damage+" hitpoints. Current hitpoints: "+newHitPoints)
       Dragon(newHitPoints, true)
     }
   }
